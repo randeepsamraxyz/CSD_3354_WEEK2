ebd2f880e9cb977452740adf63977c57b3407d36
randeepsamraxyz <39273992+randeepsamraxyz@users.noreply.github.com>
2019-01-17 17:08:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new List<string>();
+            dogs.Add("Bichon");
+            dogs.Add("Poodle");
+            dogs.Add("Libra");
+
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine(dog + " ");
+           }
+            Console.WriteLine(dogs[1]);
+
+            int[] a1 = new int[] { 1, 3, 5, 7, 9, 11 };
+            foreach (int i in a1)
+                {
+                Console.WriteLine(i +"the value of i is {0} ");
+            }
+
+        }
+    }
+}
